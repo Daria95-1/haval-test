3d8bb35ce505d6de48e76294c1450dd593d05fbe
Daria Basynina <dariabasynina@gmail.com>
2026-05-15 21:25:41 +0300

    fix layout bugs

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,23 +1,6 @@
 import Swiper from 'swiper/bundle'
 import 'swiper/css/bundle'
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   document.querySelectorAll('.car-card__image-swiper').forEach((swiperEl) => {
-//     const paginationEl =
-//       swiperEl.parentElement.querySelector('.swiper-pagination')
-
-//     new Swiper(swiperEl, {
-//       slidesPerView: 1,
-//       spaceBetween: 0,
-//       loop: true,
-//       pagination: {
-//         el: paginationEl,
-//         clickable: true,
-//       },
-//     })
-//   })
-// })
-
 document.querySelectorAll('.car-card__image-swiper').forEach((swiperEl) => {
   const paginationEl = swiperEl
     .closest('.car-card')

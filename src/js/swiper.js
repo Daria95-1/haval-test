5c68e3859ff02a4465c74c8ca4ff9ca36f5dac6f
Daria Basynina <dariabasynina@gmail.com>
2026-05-15 21:43:39 +0300

    fix swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,15 +1,19 @@
-document.querySelectorAll('.car-card__image-swiper').forEach((swiperEl) => {
-  const paginationEl = swiperEl
-    .closest('.car-card')
-    .querySelector('.car-card__pagination')
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.car-card__image-swiper').forEach((swiperEl) => {
+    const paginationEl = swiperEl
+      .closest('.car-card')
+      .querySelector('.car-card__pagination')
 
-  new Swiper(swiperEl, {
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-    pagination: {
-      el: paginationEl,
-      clickable: true,
-    },
+    if (paginationEl) {
+      new Swiper(swiperEl, {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        pagination: {
+          el: paginationEl,
+          clickable: true,
+        },
+      })
+    }
   })
 })
